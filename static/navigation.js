794ec78c67751610b7fd30b9db6bc4acbf617f5b
Peter Lyons <pete@peterlyons.com>
2022-12-14 10:42:47 -0500

    Trim down navigation JS a bit with click on nav.menu

--- static/navigation.js
+++ static/navigation.js
@@ -1,11 +1,8 @@
 /* global document */
-function closeNav() {
-  nav.classList.remove("open");
-}
 const nav = document.querySelector("nav.menu");
 document
   .querySelector("button.open-nav")
   .addEventListener("click", () => nav.classList.toggle("open"));
-document.querySelector("button.close-nav").addEventListener("click", closeNav);
-document.querySelector("nav.menu").addEventListener("click", closeNav);
-window.addEventListener("hashchange", closeNav);
+document
+  .querySelector("nav.menu")
+  .addEventListener("click", () => nav.classList.remove("open"));
