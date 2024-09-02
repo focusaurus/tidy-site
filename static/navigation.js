1c34accd1af1ab49a39437b92e6769b5fbe2b240
Peter Lyons <pete@peterlyons.com>
2024-09-02 09:51:23 -0400

    fix open nav button hide/show

--- static/navigation.js
+++ static/navigation.js
@@ -1,8 +1,11 @@
 /* global document */
 const nav = document.querySelector("nav.menu");
-document
-  .querySelector("button.open-nav")
-  .addEventListener("click", () => nav.classList.toggle("open"));
-document
-  .querySelector("nav.menu")
-  .addEventListener("click", () => nav.classList.remove("open"));
+const buttonOpenNav = document.querySelector("button.open-nav");
+buttonOpenNav.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  buttonOpenNav.classList.toggle("open");
+});
+document.querySelector("nav.menu").addEventListener("click", () => {
+  nav.classList.remove("open");
+  buttonOpenNav.classList.toggle("open");
+});
